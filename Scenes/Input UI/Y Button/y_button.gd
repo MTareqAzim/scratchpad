7be55533fbc1cc7d07e3eacbe2e5b2ad48cf2861
Tareq Anuar <MTareqAzim@gmail.com>
2019-10-17 16:14:31 +0800

    Buttons animated.

---
+++ Scenes/Input UI/Y Button/y_button.gd
@@ -0,0 +1,12 @@
+extends Control
+
+onready var animation_player = $Sprite/AnimationPlayer
+
+func _input(event):
+	if event.is_action_pressed("ui_y"):
+		animation_player.queue("Press")
+		animation_player.queue("Pressed")
+
+	if event.is_action_released("ui_y"):
+		animation_player.queue("Depress")
+		animation_player.queue("Idle")
