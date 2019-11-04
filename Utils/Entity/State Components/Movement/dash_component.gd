69561700334d78a302ceee09e027831e84d63c12
Tareq Anuar <MTareqAzim@gmail.com>
2019-11-04 18:12:03 +0800

    Refined dash component.

--- Utils/Entity/State Components/Movement/dash_component.gd
+++ Utils/Entity/State Components/Movement/dash_component.gd
@@ -5,6 +5,7 @@ onready var _look_direction : LookDirection = get_node(look_direction)
 export (NodePath) var look_direction
 export (int) var dash_distance := 300
 export (float) var dash_duration := 0.5
+export (String) var next_state
 
 
 func enter() -> void:
@@ -21,4 +22,4 @@ func _set_values() -> void:
 	$Timer.set_wait_time(dash_duration)
 
 func _on_Timer_timeout():
-	component_state.finished("previous")
+	component_state.finished(next_state)
