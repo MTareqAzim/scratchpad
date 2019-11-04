2136ab05e7d1cd89f763e782bb84303f0643ebf5
Tareq Anuar <MTareqAzim@gmail.com>
2019-11-04 17:49:59 +0800

    Added back roll state.

--- Utils/Entity/State Components/Movement/crouch_height.gd
+++ Utils/Entity/State Components/Movement/crouch_height.gd
@@ -5,13 +5,7 @@ onready var _body : KinematicBody2P5D = get_node(body)
 export (NodePath) var body
 export (int) var crouch_height := 50
 
-var _prev_height : int
-
-
-func _ready() -> void:
-	_prev_height = _body.get_height()
-
 
 func enter() -> void:
 	$"Enter Height".new_value = crouch_height
-	$"Exit Height".new_value = _prev_height
+	$"Exit Height".new_value = _body.get_height()
