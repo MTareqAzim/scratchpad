28fb3501880d10f5e7e98035f1caefde0b26b09c
Tareq Anuar <MTareqAzim@gmail.com>
2019-11-04 15:30:17 +0800

    Fix event echo bug.

--- Utils/Input Handler/Action Map/action_map.gd
+++ Utils/Input Handler/Action Map/action_map.gd
@@ -7,6 +7,9 @@ export (String) var mapped_action
 func map(event : InputEvent) -> InputEvent:
 	if not event.is_action(action):
 		return null
+
+	if event.is_echo():
+		return null
 	
 	var mapped_event = InputEventAction.new()
 	mapped_event.action = mapped_action
