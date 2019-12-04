7b68d3a8ed74631fbff856b160d75b0fbc92d517
Tareq Anuar <MTareqAzim@gmail.com>
2019-12-04 17:31:29 +0800

    Apply args to equals transition.

--- Utils/State Machine/State/State Components/Node State Components/Update/Transitions/equals_transition_state_component.gd
+++ Utils/State Machine/State/State Components/Node State Components/Update/Transitions/equals_transition_state_component.gd
@@ -2,11 +2,18 @@ extends NodeStateComponent
 class_name EqualsTransitionStateComponent, "equals_transition.png"
 
 export (String) var FUNCTION_NAME
-export (String) var equals
+export (bool) var has_args
+export (Array) var args
+export (Array) var equals
 export (String) var NEXT_STATE
 
 
 func update(delta: float) -> void:
-	var variable = str(node.call(FUNCTION_NAME))
-	if variable == equals:
+	var variable
+	if has_args:
+		variable = node.callv(FUNCTION_NAME, args)
+	else:
+		variable = node.call(FUNCTION_NAME)
+	
+	if variable == equals[0]:
 		component_state.finished(NEXT_STATE)
