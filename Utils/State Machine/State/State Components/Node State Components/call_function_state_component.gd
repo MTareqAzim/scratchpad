9d6935c92d754840e43e4df2272fe732e858e389
Tareq Anuar <MTareqAzim@gmail.com>
2019-12-10 19:13:12 +0800

    Added warning to call function state component.

--- Utils/State Machine/State/State Components/Node State Components/call_function_state_component.gd
+++ Utils/State Machine/State/State Components/Node State Components/call_function_state_component.gd
@@ -1,9 +1,14 @@
+tool
 extends NodeStateComponent
 class_name CallFunctionStateComponent, "fun.png"
 
 export (String) var FUNCTION_NAME
 export (bool) var has_args := true
 export (Array) var args
+
+
+func _get_configuration_warning() -> String:
+	return "Don't use this component. Use it's subclasses."
 
 
 func call_function() -> void:
