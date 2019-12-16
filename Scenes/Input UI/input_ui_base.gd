c1b81e48885fe349e3785756f2ec731d4eb9f11a
Tareq Anuar <MTareqAzim@gmail.com>
2019-12-16 19:47:47 +0800

    Add input ui.

--- Scenes/Input UI/input_ui_base.gd
+++ Scenes/Input UI/input_ui_base.gd
@@ -5,7 +5,7 @@ onready var b_text = $BTextBox
 onready var x_text = $XTextBox
 onready var y_text = $YTextBox
 
-func _on_StateMachine_state_changed(states_stack):
+func _on_State_Machine_state_changed(states_stack):
 	var new_state = states_stack[0]
 	
 	a_text.call_deferred("state_changed", new_state)
