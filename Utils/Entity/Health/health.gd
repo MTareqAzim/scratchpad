c10030f4b702c4639ad8ef4227ffda39a26f866e
Tareq Anuar <MTareqAzim@gmail.com>
2019-12-16 17:33:33 +0800

    Upgraded health node.

--- Utils/Entity/Health/health.gd
+++ Utils/Entity/Health/health.gd
@@ -1,16 +1,29 @@
 extends Node
-class_name Health
+class_name Health, "health.png"
 
 signal health_changed(health)
+signal healed
+signal damaged
 
 export (int, 0, 2147483647) var max_health
 
-onready var _health : int = max_health
+onready var _health : int = max_health setget _set_health, get_health
+
+
+func heal_health(amount: int) -> void:
+	if amount > 0:
+		_set_health(_health + amount)
+		emit_signal("healed")
 
 
 func damage_health(amount: int) -> void:
+	if amount > 0:
+		_set_health(_health - amount)
+		emit_signal("damaged")
+
+
+func _set_health(new_health: int) -> void:
 	var previous_health = _health
-	var new_health = _health - amount
 	_health = int(clamp(new_health, 0, max_health))
 	
 	if _health != previous_health:
