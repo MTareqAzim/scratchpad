0fe808e08b5b864a41caabbae5764d761e0d9340
Tareq Anuar <MTareqAzim@gmail.com>
2020-01-24 09:45:55 +0800

    Completed depth sorter logic.

--- Utils/Depth Sorting/depth_sort.gd
+++ Utils/Depth Sorting/depth_sort.gd
@@ -10,12 +10,16 @@ func get_body() -> Node2D:
 	return _body
 
 
+func set_body_z_index(new_z_index: int) -> void:
+	_body.set_z_index(new_z_index)
+
+
 func get_overlapping_depth_sorts() -> Array:
 	return get_overlapping_areas()
 
 
-func set_body_z_index(new_z_index: int) -> void:
-	_body.set_z_index(new_z_index)
+func in_front_of(depth_sort: DepthSort) -> bool:
+	return true
 
 
 func _set_body(new_body: Node2D) -> void:
--- Utils/Depth Sorting/depth_sorter.gd
+++ Utils/Depth Sorting/depth_sorter.gd
@@ -29,18 +29,42 @@ func _append_depth_sorts(node: Node) -> void:
 func _calculate_graph() -> DirectedGraph:
 	var digraph = DirectedGraph.new(_depth_sorts.size())
 	
+	for vertex in _depth_sorts.size():
+		var depth_sort = _depth_sorts[vertex]
+		for other_depth_sort in depth_sort.get_overlapping_depth_sorts():
+			if depth_sort.in_front_of(other_depth_sort):
+				var vertex_to = _depth_sorts.find(other_depth_sort)
+				digraph.add_edge(vertex, vertex_to)
+	
+	var tarjan = TarjanAlgorithm.new(digraph)
+	var strongly_connected_components = tarjan.get_strongly_connected_components()
+	var cycles = []
+	
+	for scc in strongly_connected_components:
+		if scc.size() > 1:
+			cycles.append(scc)
+	
+	for cycle in cycles:
+		var vertex_to = cycle[0]
+		for vertex in cycle:
+			vertex_to = max(vertex_to, vertex)
+		var index = cycle.find(vertex_to)
+		var vertex = cycle[(index + 1) % cycle.size()]
+		digraph.remove_edge(vertex, vertex_to)
+	
 	return digraph
 
 
 func _calculate_z_indexes() -> Array:
-	var z_indexes = []
-	
-	for i in _depth_sorts.size():
-		z_indexes.append(i)
+	var z_indexes = DirectedGraphSorter.kahn_sort(_depth_graph)
+	z_indexes.invert()
 	
 	return z_indexes
 
 
 func _set_z_index_of_nodes() -> void:
+	if _z_indexes == []:
+		return
+	
 	for i in _depth_sorts.size():
 		_depth_sorts[i].set_body_z_index(_z_indexes[i])
