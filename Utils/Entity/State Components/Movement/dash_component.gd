7cfc379efb703dff3056b1f218977039cf831cbb
Tareq Anuar <MTareqAzim@gmail.com>
2020-01-26 00:15:11 +0800

    Updated dash to use variables.

--- Utils/Entity/State Components/Movement/dash_component.gd
+++ Utils/Entity/State Components/Movement/dash_component.gd
@@ -2,12 +2,14 @@ extends EntityStateComponent
 
 onready var _timer : Timer = $Timer
 
-export (String) var look_direction_key
-export (int) var dash_distance := 300
-export (float) var dash_duration := 0.5
+export (String) var look_direction_key := "look"
+export (String) var dash_distance_key = "dash_distance"
+export (String) var dash_duration_key = "dash_duration"
 export (String) var next_state
 
 var _look_direction : LookDirection
+var _dash_distance : int
+var _dash_duration : float
 
 
 func enter() -> void:
@@ -29,12 +31,17 @@ func assign_dependencies() -> void:
 	_look_direction = component_state.get_dependency(look_direction_key)
 
 
+func assign_variables() -> void:
+	_dash_distance = component_state.get_variable(dash_distance_key)
+	_dash_duration = component_state.get_variable(dash_duration_key)
+
+
 func _set_values() -> void:
-	var speed : float = dash_distance / dash_duration
+	var speed : float = _dash_distance / _dash_duration
 	var direction : Vector2 = _look_direction.get_look_direction()
 	
 	$"Enter Velocity 2D".args = [direction * speed]
-	$Timer.set_wait_time(dash_duration)
+	$Timer.set_wait_time(_dash_duration)
 
 
 func _on_Timer_timeout():
